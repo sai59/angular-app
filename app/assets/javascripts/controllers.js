86c4b712530ffb910f46b56127077710f1dd386e
sai59 <saikrishnakota@hotmail.com>
2014-07-24 16:19:53 +0530

    Done with qservice

--- app/assets/javascripts/controllers.js
+++ app/assets/javascripts/controllers.js
@@ -1,34 +1,50 @@
-//var todoControllers = angular.module('todoControllers', []);
 var todoApp = angular.module('todoApp',['todoControllers', 'todoServices']);
 var todoControllers = angular.module('todoControllers',[]);
 var todoServices = angular.module('todoServices',[]);
 
-todoServices.service('todoService', ['$http', function($http){
-  this.getTodos = function(callbackFunc){
-    $http.get('insiders/todos').success(function(data){
-      callbackFunc(data);
-    });
-  }
+todoServices.factory('todoService', ['$http', '$q', function($http, $q){
+  return {
 
-  this.addTodo = function(data, callbackFunc){
-    $http.post('insiders/todos/new', data).success(function(data){
-      callbackFunc(data);
-    });
+    getTodos: function(){
+      return $http.get('insiders/todos')
+        .then(function(response){
+          return response.data;
+        },function(response){
+          return "invalid response";
+        });
+    },
+
+    addTodo: function(todo){
+      return $http.post('insiders/todos/new', todo)
+        .then(function(response){
+          return response.data;
+        },function(response){
+          return "invalid response";
+        });
+    }
+
   }
 }]);
 
-todoControllers.controller('TodoListCtrl', ['$scope', 'todoService', function($scope, todoService){
+todoControllers.controller('TodoListCtrl', ['$scope', '$q', 'todoService', function($scope, $q, todoService){
   $scope.todos = null;
-  todoService.getTodos(function (data) {
+
+  todoService.getTodos().then(function(data) {
     $scope.todos = data;
+  }, function(error) {
+    console.log(error);
   });
+
   $scope.submit = function() {
     if ($scope.description) {
-      var data = {description: $scope.description}
-      todoService.addTodo(data, function(dataresponse) {
-        $scope.todos.push(dataresponse);
-        $scope.description = '';
-      });
+      var todo = {description: $scope.description}
+      todoService.addTodo(todo)
+        .then(function(data){
+            $scope.todos.push(data);
+            $scope.description = '';
+          },function(error){
+            console.log(error);
+          });
     }
   }
 }]);
