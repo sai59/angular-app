0dbb100f35a7cf8e097708b6bd0b97a7f6ade61e
sai59 <saikrishnakota@hotmail.com>
2014-08-30 15:29:13 +0530

    Save fb long lived access token

--- app/lib/facebook.rb
+++ app/lib/facebook.rb
@@ -10,9 +10,16 @@ class Facebook
     }
   end
 
-  def long_lived_access_token(token)
-    @options.merge!({:fb_exchange_token => token})
-    self.class.get("/oauth/access_token", @options)
+  def long_lived_access_token(current_user, token)
+    # @options.merge!({:fb_exchange_token => token})
+    # self.class.get("/oauth/access_token", @options)
+    response = HTTParty.get("https://graph.facebook.com/oauth/access_token?client_id=#{Settings.facebook.app_id}&client_secret=#{Settings.facebook.app_secret}&grant_type=fb_exchange_token&fb_exchange_token=#{token}")
+    if response.code == 200
+      current_user.update_attribute(:fb_long_lived_token, response.parsed_response.split('&')[0].split('=')[1])
+      response
+    else
+      response
+    end
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20140728090913) do
+ActiveRecord::Schema.define(:version => 20140830083505) do
 
   create_table "todos", :force => true do |t|
     t.text     "description"
@@ -25,8 +25,9 @@ ActiveRecord::Schema.define(:version => 20140728090913) do
     t.string   "email"
     t.string   "password"
     t.string   "access_token"
-    t.datetime "created_at",   :null => false
-    t.datetime "updated_at",   :null => false
+    t.datetime "created_at",          :null => false
+    t.datetime "updated_at",          :null => false
+    t.text     "fb_long_lived_token"
   end
 
 end
